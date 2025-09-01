05e80a7d87a06d323ed618a930f7c1112874745c
Kamesuta <kamesuta@gmail.com>
2025-09-01 22:35:28 +0900

    remove: Helloコマンド削除

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,9 +1,7 @@
 import { InteractionBase } from "./base/interaction_base.js";
-import helloCommands from "./hello_command/commands.js";
 import vcSummaryCommands from "./vc_summary_command/commands.js";
 
 const commands: InteractionBase[] = [
-  ...helloCommands,
   ...vcSummaryCommands,
   // Add other command group lists here
 ];
